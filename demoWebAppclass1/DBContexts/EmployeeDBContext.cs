888de10aaba2822385d97e0832e2966a331e914c
DESKTOP-4U54Q3V\User <hadiuzzaman15-13397@diu.edu.bd>
2022-02-21 21:21:12 +0600

    initial implementation

---
+++ demoWebAppclass1/DBContexts/EmployeeDBContext.cs
@@ -0,0 +1,18 @@
+﻿using demoWebAppclass1.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace demoWebAppclass1.DBContexts
+{
+    public class EmployeeDBContext : DbContext
+    {
+        public EmployeeDBContext(DbContextOptions<EmployeeDBContext> options)
+            : base(options)
+        {
+
+        }
+
+        public DbSet<Employee> Employees { get; set; } 
+
+
+    }
+}
---
+++ demoWebAppclass1/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace demoWebAppclass1.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
